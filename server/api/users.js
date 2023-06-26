0562f4c877ad9b2a6b6791162ba782c8db7e2841
Jake <jakebajo@gmail.com>
2023-06-26 14:54:35 -0700

    added small changes to get code working

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { models: { User }} = require('../db')
+const { models: { User }} = require('../db/db')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
