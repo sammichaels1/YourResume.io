109fab8ac75c9715fea221e0221d6e5ededa3f90
Your Nameexit <sjmichaels95@gmail.com>
2023-06-26 14:16:43 -0500

    added new files

---
+++ server/api/index.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+module.exports = router;
+
+
+
+router.use('/users', require('./users'))
+router.use('/movies', require('./movies'))
+router.use('/tvshows', require('./TVSHOWS'))
+router.use('/gpt', require('./gpt'))
+
+
+
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
